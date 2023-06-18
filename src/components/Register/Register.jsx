6244cb768b2260f2f16633ea90cb365ad5b5c6b1
tawrat41 <tawratur.rashid@gmail.com>
2023-06-19 01:42:44 +0600

    slight css

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Register.css'
 
 const Register = () => {
     return (
         <div>
-            <h1>Register</h1>
+            <h1>Please Register</h1>
+            <form>
+                <input className='email-field' type="email" name="email" id="email" placeholder='your email' /><br />
+                <input className='pass-field' type="password" name="password" id="password" placeholder='enter password'/><br />
+                <br />
+                <input className='register-btn' type="submit" value="Register" />
+
+            </form>
+
+
         </div>
     );
 };
